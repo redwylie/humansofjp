c6353285187645f62e25dfce2f893fe7c0a2054e
Mark Wylie <mwylie@mgh.harvard.edu>
2017-08-15 15:19:14 -0400

    added the go top footer

--- assets/scripts/humansofjp/common.js
+++ assets/scripts/humansofjp/common.js
@@ -6,20 +6,22 @@ function setProps(){
 	var currentYear = new Date().getFullYear();
 	$("#year").text(currentYear);
 
-	// DIGITILER SPECIFIC
-	$('.hidden-info').hide();
-	$('.show-info').click(function() {
-		$('.hidden-info').toggle();
-	});
+	// Show or hide the sticky footer button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.go-top').fadeIn(200);
+        } else {
+            $('.go-top').fadeOut(200);
+        }
+    });
+    
+    // Animate the scroll to top
+    $('.go-top').click(function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({scrollTop: 0}, 300);
+    });
 
-	var width = $(window).width();
-	width = width - 30;
-  	$('#openseadragon').css({'width' : width});
-
-	var height = $(window).height();
-	height = height - 80;
-	$('#openseadragon').css({'height' : height});
-	// END DIGITILER SPECIFIC
 }
 
 function goBack() {
